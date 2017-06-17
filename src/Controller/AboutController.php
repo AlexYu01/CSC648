91f7068ad7fcf5f2a8720881823671299a7bdce6
--quiet <su17g05@ip-172-31-3-78.us-west-1.compute.internal>
2017-06-17 03:35:15 +0000

    Setup Cakephp Framework

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class AboutController extends AppController
+{
+    public function index()
+    {       
+    }
+    
+    public function andy(){
+        
+    }
+}
