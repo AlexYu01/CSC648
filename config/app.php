84cb3fe8a41b941ac0066d6ede7de82befd6aaaa
HAOTIAN ZHANG <ivanzhanght@gmail.com>
2017-06-21 15:16:29 -0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -229,9 +229,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'su17g05',
-            'password' => 'csc648su17g05',
-            'database' => 'su17g05',
+            'username' => 'hzhang3',
+            'password' => '915588250',
+            'database' => 'student_hzhang3',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
