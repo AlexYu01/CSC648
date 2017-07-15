2bf6441a3778448b70208e12e789dc140419e834
AlexYu01 <tengalexyu@gmail.com>
2017-07-15 15:52:56 -0700

    Adjusting routes

--- config/routes.php
+++ config/routes.php
@@ -50,14 +50,14 @@ Router::scope('/', function (RouteBuilder $routes) {
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
     //$routes->connect('/', ['controller' => 'About', 'action' => 'index', 'home']);
-    $routes->connect('/', ['controller' => 'Homepage', 'action' => 'index', 'home']);
+    $routes->connect('/', ['controller' => 'Homepage', 'action' => 'display', 'index', 'home']);
     $routes->connect('/search/*', ['controller' => 'Results', 'action' => 'search']);
 
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
-    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display','home']);
+    //$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display','home']);
 
     /**
      * Connect catchall routes for all controllers.
