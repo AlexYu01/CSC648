18d71e5ada6ec7c46b8415d02187b129c39e005f
AlexYu01 <tengalexyu@gmail.com>
2017-07-15 20:33:54 -0700

    Drop down

--- src/Controller/ResultsController.php
+++ src/Controller/ResultsController.php
@@ -26,14 +26,23 @@ class ResultsController extends AppController {
         $session = $this->request->session();
         $searchTerm = $session->read('searchTerm');
         $searchGenre = $session->read('searchGenre');
-        $results = $this->Media->find('all', [
-            'conditions' => ['Media.type_id' => 1, 'Media.genre_id LIKE' =>
-                '%' . $searchGenre . '%', 'OR' =>
-                ['Media.media_title LIKE' => '%' . $searchTerm . '%',
-                    'Media.media_desc LIKE' => '%' . $searchTerm . '%']
-            ]
-        ]);
 
+        if (strlen($searchGenre) > 0) {
+            $results = $this->Media->find('all', [
+                'conditions' => ['Media.type_id' => 1,
+                    'Media.genre_id' => $searchGenre, 'OR' =>
+                    ['Media.media_title LIKE' => '%' . $searchTerm . '%',
+                        'Media.media_desc LIKE' => '%' . $searchTerm . '%']
+                ]
+            ]);
+        } else {
+            $results = $this->Media->find('all', [
+                'conditions' => ['Media.type_id' => 1, 'OR' => 
+                    ['Media.media_title LIKE' => '%' . $searchTerm . '%',
+                        'Media.media_desc LIKE' => '%' . $searchTerm . '%']
+                ]
+            ]);
+        }
 
         if ($this->request->is('get')) {
             $this->request->data('search', $searchTerm);
@@ -42,8 +51,7 @@ class ResultsController extends AppController {
 
         if ($results->isEmpty()) {
             $results = $this->Media->find('all', ['conditions' =>
-                ['Media.type_id' => 1, 'Media.genre_id LIKE' =>
-                    '%' . $searchGenre . '%']]);
+                ['Media.type_id' => 1]]);
         }
 
         $genreList = $this->MediaGenres->find('list', ['keyField' => 'genre_id',
