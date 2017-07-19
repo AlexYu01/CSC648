b451ffe01a651e6752206a9d6ce48161e2b06f12
AlexYu01 <tengalexyu@gmail.com>
2017-07-19 14:00:12 -0700

    Add documentation and orginaztion
    - Added documentation for controllers related to search.
    
    - Made the SearchForm variable , searchFields into a singleton, so it is not created repeatedly with every call to searchBar().

--- src/Controller/ResultsController.php
+++ src/Controller/ResultsController.php
@@ -8,6 +8,7 @@ use Cake\Network\Exception\NotFoundException;
 
 class ResultsController extends AppController {
 
+    // limit 4 results per page
     public $paginate = [
         'limit' => 4
     ];
@@ -28,6 +29,7 @@ class ResultsController extends AppController {
         $searchTerm = $session->read('searchTerm');
         $searchGenre = $session->read('searchGenre');
 
+        // user searched with genre and a term
         if (strlen($searchGenre) > 0) {
             $results = $this->Media->find('all', [
                 'conditions' => ['Media.type_id' => 1,
@@ -36,6 +38,15 @@ class ResultsController extends AppController {
                         'Media.media_desc LIKE' => '%' . $searchTerm . '%']
                 ]
             ]);
+
+            /* Raw query form for above
+             * SELECT *
+             * FROM media
+             * WHERE type_id = 1 AND genre_id = $searchGenre AND 
+             * (media_title LIKE %$searchTerm% OR media_desc LIKE %$searchTerm%;
+             */
+            
+        // user only entered a term
         } else {
             $results = $this->Media->find('all', [
                 'conditions' => ['Media.type_id' => 1, 'OR' =>
@@ -43,25 +54,62 @@ class ResultsController extends AppController {
                         'Media.media_desc LIKE' => '%' . $searchTerm . '%']
                 ]
             ]);
+
+            /* Raw query form for above
+             * SELECT *
+             * FROM media
+             * WHERE type_id = 1 AND 
+             * (media_title LIKE %$searchTerm% OR media_desc LIKE %$searchTerm%;
+             */
         }
 
+
+        // User search returned empty. Give them top sellers under the genre 
+        // they chose if applicable else return top sellers in all genres.
+         
+        if ($results->isEmpty()) {
+            if (strlen($searchGenre) > 0) {
+                $results = $this->Media->find('all', [
+                    'conditions' => ['Media.type_id' => 1,
+                        'Media.genre_id' => $searchGenre, 'OR' =>
+                        ['Media.media_title LIKE' => '%' . $searchTerm . '%',
+                            'Media.media_desc LIKE' => '%' . $searchTerm . '%']
+                    ]
+                ])
+                        ->order(['Media.sold_count' => 'DSC']);
+                
+                /* Raw query form for above
+                 * SELECT *
+                 * FROM media
+                 * WHERE type_id = 1 AND genre_id = $searchGenre
+                 * ORDER BY Media.sold_count DESC;
+                 */
+            } else {
+                $results = $this->Media->find('all', ['conditions' =>
+                    ['Media.type_id' => 1]])
+                        ->order(['Media.sold_count' => 'DSC']);
+
+                /* Raw query form for above
+                 * SELECT *
+                 * FROM media
+                 * WHERE type_id = 1
+                 * ORDER BY Media.sold_count DESC;
+                 */
+            }
+        }
+        
+        // fills in search fields with user's input on results page
         if ($this->request->is('get')) {
             $this->request->data('search', $searchTerm);
             $this->request->data('dropDown', $searchGenre);
         }
 
-        if ($results->isEmpty()) {
-            $results = $this->Media->find('all', ['conditions' =>
-                ['Media.type_id' => 1]]);
-        }
-
-        $genreList = $this->MediaGenres->find('list', ['keyField' => 'genre_id',
-                            'valueField' => 'genre_name'])
-                        ->hydrate(false)->toArray();
-
         try {
+            // send results as paginated to view
             $this->set('results', $this->paginate($results));
         } catch (NotFoundException $e) {
+            // user tried accessing a page that does not exist or run search 
+            // on a page > 1
             return $this->redirect(['controller' => 'Results', 'action' => 'search']);
         }
     }
