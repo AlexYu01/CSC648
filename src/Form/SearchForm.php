99ea5953ea8d5f34f9d5bdfe852b3d2838fc3a87
AlexYu01 <tengalexyu@gmail.com>
2017-07-22 13:26:57 -0700

    Moved search validation onto the form

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -6,26 +6,33 @@ use Cake\Form\Form;
 use Cake\Form\Schema;
 use Cake\Validation\Validator;
 
+/**
+ * SearchForm is a model-less form as search is not tied to any database tables.
+ * Do not create forms like SearchForm when creating registration/login forms; 
+ * they will use the UsersTable.php class for validation.  
+ */
 class SearchForm extends Form {
 
-    protected function _buildSchema(Schema $schema) {
-        return $schema->addField('search', 'string')
-                        ->addField('dropDown', ['type' => 'string']);
+    protected function _buildSchema( Schema $schema ) {
+        return $schema->addField( 'search', 'string' )
+                        ->addField( 'dropDown', 'string' );
         // ->addField('body', ['type' => 'text']);
     }
 
-    protected function _buildValidator(Validator $validator) {
-        /* return $validator->add('Search', 'length', [
-          'rule' => ['minLength', 1],
-          'message' => 'Please enter something'
-          ])->add('email', 'format', [
-          'rule' => 'email',
-          'message' => 'A valid email address is required',
-          ]) */;
+    protected function _buildValidator( Validator $validator ) {
+        $validator
+                // allow the search field to be empty
+                ->allowEmpty( 'search' )
+                // calls maxLength method from Validator class to ensure length <= 30
+                ->add( 'search', 'length',
+                        ['rule' => ['maxLength', 30], 'message' => 'Please no more than 30 characters'] )
+                // calls custom method from Validator class to run a custom regex
+                ->add( 'search', 'validChars',
+                        ['rule' => ['custom', '/^[[:alnum:][:blank:]]+$/'], 'message' => 'Please enter only alphabets, numbers, or spaces'] );
         return $validator;
     }
 
-    protected function _execute(array $data) {
+    protected function _execute( array $data ) {
         // Send an email.
         return true;
     }
