81f700f526630c689139a1b6d677df0526c3f4c9
AlexYu01 <tengalexyu@gmail.com>
2017-07-25 15:48:57 -0700

    Removing files for upload

--- config/routes.php
+++ config/routes.php
@@ -52,7 +52,6 @@ Router::scope('/', function (RouteBuilder $routes) {
     //$routes->connect('/', ['controller' => 'About', 'action' => 'index', 'home']);
     $routes->connect('/', ['controller' => 'Homepage', 'action' => 'index']);
     $routes->connect('/search', ['controller' => 'Results', 'action' => 'search']);
-    $routes->connect('/upload', ['controller' => 'Media', 'action' => 'upload']);
 
 
     /**
