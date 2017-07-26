15e83259acd2009731b19073a7b123a549ea3902
AlexYu01 <tengalexyu@gmail.com>
2017-07-25 17:34:54 -0700

    Fixes to MediaController

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -18,16 +18,16 @@ class MediaController extends AppController {
         if ( $this->request->is( 'post' ) ) {
             $mediaData = $this->request->data['file']['tmp_name'];
             $mediaContent = file_get_contents( $_FILES['file']['tmp_name'] );
-            echo $mediaContent;
+            /*echo $mediaContent;
             echo $mediaData;
-            echo gettype( $mediaData );
+            echo gettype( $mediaData );*/
             $mediaType = pathinfo( $mediaData, PARTHINFO_EXTENSION );
             $this->request->data['media_data'] = $mediaContent;
-            $this->reuqest->data['media_ext'] = $mediaType;
+            $this->request->data['media_ext'] = $mediaType;
 
             $newMedia = $this->Media->patchEntity( $newMedia,
                     $this->request->data );
-            if ( $this->Media - save( $newMedia ) ) {
+            if ( $this->Media->save( $newMedia ) ) {
                 $this->Flash->success( __( 'The media  has been saved.' ) );
             } else {
                 $this->Flash->error( __( 'The media could not be saved. Please, try again.' ) );
