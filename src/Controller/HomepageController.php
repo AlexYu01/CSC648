00e466cb37b01806c6ce372011b333077324c9f9
Victor C <victorcheng@Victors-MacBook-Pro.local>
2017-07-26 21:43:21 -0700

    homepage controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,14 +5,35 @@ namespace App\Controller;
 use App\Controller\AppController;
 
 class HomepageController extends AppController {
-
-    public function initialize() {
-        parent::initialize();
-    }
-
-    public function index() {
-
-        $this->searchBar(); // inherited from AppController
-    }
-
+	public function initialize() {
+		parent::initialize ();
+		
+	}
+	public function index() {
+		
+// 		$this->searchBar(); // inherited from AppController
+		
+		$this->loadModel ( 'MediaGenres' );
+		$mgResults = $this->MediaGenres->find ( 'all' )->toArray ();
+		$this->set ('genresData', $mgResults);
+		// pr($mgResults);
+		
+		$this->loadComponent ( 'Paginator' );
+		$this->loadModel ( 'Media' );
+		$results = $this->Media->find ( 'all', [ 
+				'conditions' => [ 
+						'Media.type_id' => 1 
+				] 
+		] );
+		
+		$this->paginate = [ 
+				'limit' => 6,
+				'contain' => [ 
+						'mediagenres'  
+				] 
+		];
+		$images = $this->paginate ( $results );
+		
+		$this->set ( 'productData', $images );
+	}
 }
